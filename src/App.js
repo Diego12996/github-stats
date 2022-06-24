b7c06ddfaf695e1f003aaf20c5267af89de9e033
emachipana <emachipana0102@gmail.com>
2022-06-24 16:56:33 -0500

    ADD: test card user in to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import CardUser from "./components/CardUser";
 
 function App() {
-  return <CardUser />
+  return (
+    <>
+      <CardUser 
+        src="https://avatars.githubusercontent.com/u/94902863?v=4"
+        name="Enmanuel Chipana"
+        username="emachipana"
+        isFavorite
+      />
+      <CardUser 
+        src="https://avatars.githubusercontent.com/u/91627176?v=4"
+        name="Lucho Zelada"
+      />
+    </>
+  )
 }
 
 export default App;
