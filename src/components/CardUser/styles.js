80f03960c6311d49b504732646c96cb94a4cc5af
emachipana <emachipana0102@gmail.com>
2022-06-24 14:27:56 -0500

    ADD: styles to card user

--- src/components/CardUser/styles.js
+++ src/components/CardUser/styles.js
@@ -1,6 +1,9 @@
 import styled from "@emotion/styled";
+import { colors, fonts } from "../../styles";
+import { BsStarFill } from "react-icons/bs";
 
 export const Container = styled.div`
+  margin: 1rem;
   width: 300px;
   height: 56px;
   background-color: white;
@@ -9,6 +12,13 @@ export const Container = styled.div`
   padding: 8px 12px;
   display: flex;
   gap: 10px;
+  font-family: ${fonts.primary};
+  align-items: center;
+  justify-content: space-between;
+  transition: .3s ease-in;
+  &:hover {
+    transform: translateY(-3px);
+  }
 `;
 
 export const Avatar = styled.img`
@@ -17,3 +27,31 @@ export const Avatar = styled.img`
   border-radius: 50%;
 `;
 
+export const Name = styled.h2`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 17px;
+  line-height: 20px;
+  color: ${colors.gray[900]};
+`;
+
+export const Username = styled.p`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 15px;
+  color: ${colors.gray[800]};
+`;
+
+export const Info = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+export const Star = styled(BsStarFill)`
+  margin: 0 0.5rem;
+  font-size: 22px;
+  cursor: pointer;
+  color: rgba(242, 201, 76, 1);
+`;
