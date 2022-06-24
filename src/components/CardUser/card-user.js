946b6ab8430184ee8789ae072a66b1da024bac76
emachipana <emachipana0102@gmail.com>
2022-06-24 14:00:54 -0500

    ADD: partial structure of card user

---
+++ src/components/CardUser/card-user.js
@@ -0,0 +1,14 @@
+import * as Style from "./styles";
+
+function CardUser() {
+  return (
+    <Style.Container>
+      <Style.Avatar 
+        src="https://avatars.githubusercontent.com/u/94902863?v=4"
+        alt="github-avatar"
+      />
+    </Style.Container>
+  );
+};
+
+export default CardUser;
