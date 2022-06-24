674ddf39d732179de4fa409279dc64dbf719a896
emachipana <emachipana0102@gmail.com>
2022-06-24 11:02:07 -0500

    ADD: functions to followers and followings

--- src/services/github-api-service.js
+++ src/services/github-api-service.js
@@ -7,3 +7,12 @@ export function getUser(username) {
 export function getRepos(repos_url) {
   return fetch(`${BASE_URI}${repos_url}`).then((response) => response.json());
 }
+
+export function getFollowers(followers_url) {
+  return fetch(`${BASE_URI}${followers_url}`).then((response) => response.json());
+}
+
+export function getFollowings(followings_url) {
+  const url = followings_url.slice(0, -13);
+  return fetch(`${BASE_URI}${url}`).then((response) => response.json());
+}
