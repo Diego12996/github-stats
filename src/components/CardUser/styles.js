dced9e3bed8b22cd49a1e0db5f42177f5c10a70d
emachipana <emachipana0102@gmail.com>
2022-06-24 14:00:17 -0500

    ADD: first styles of Card User component

---
+++ src/components/CardUser/styles.js
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  width: 300px;
+  height: 56px;
+  background-color: white;
+  box-shadow: 2px 2px 0px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+  padding: 8px 12px;
+  display: flex;
+  gap: 10px;
+`;
+
+export const Avatar = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+`;
+
