32a51b35a5dadb1fe3a7fe1d7889f7a5a398e844
Lucho Zelada <luisdanger96@gmail.com>
2022-06-24 17:42:48 -0500

    ADD: button component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import CardUser from "./components/CardUser";
-import TitleHead from "./components/Title";
+import Button from "./components/Button";
 
 function App() {
   return (
     <>
-      <TitleHead></TitleHead>
-      <CardUser 
-        src="https://avatars.githubusercontent.com/u/94902863?v=4"
-        name="Enmanuel Chipana"
-        username="emachipana"
-        isFavorite
-      />
-      <CardUser 
-        src="https://avatars.githubusercontent.com/u/91627176?v=4"
-        name="Lucho Zelada"
-      />
+      <Button isPrimary={true}>Login </Button>
     </>
   )
 }
