ce838cded3a3e1b3f409fa28a0b523893f45ae3b
emachipana <emachipana0102@gmail.com>
2022-06-24 21:39:38 -0500

    ADD: styles to pages

---
+++ src/pages/styles.js
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  max-width: 260px;
+  margin: 1rem auto;
+`;
+
+export const Form = styled.form`
+  max-width: 250px;
+  margin: 2rem auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+`;
