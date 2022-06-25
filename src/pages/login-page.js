7eaaf03f6fc19f4ce523f9bab88fc135e1f425a2
emachipana <emachipana0102@gmail.com>
2022-06-24 23:58:20 -0500

    ADD: Login with login form

--- src/pages/login-page.js
+++ src/pages/login-page.js
@@ -1,24 +1,50 @@
+import { useState } from "react";
 import Button from "../components/Button";
 import IndividualInput from "../components/Input";
 import LinkTo from "../components/LinkTo";
 import TitleHead from "../components/Title";
+import { useAuth } from "../context/auth-context";
 import * as Style from "./styles";
 
 function LoginPage() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const { login, error } = useAuth();
+
+  const { email, password } = formData;
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData({...formData, [name]: value});
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    login(formData);
+  }
+
   return (
     <Style.Container>
       <TitleHead>Welcome to Github Stats</TitleHead>
-      <Style.Form>
+      <Style.Form onSubmit={handleSubmit}>
         <IndividualInput 
           label="Email"
           placeholder="example@gmail.com"
           type="email"
+          value={email}
+          onChange={handleChange}
         />
         <IndividualInput 
           label="Password"
           placeholder="*******"
           type="password"
+          value={password}
+          onChange={handleChange}
         />
+        {error && (<p style={{fontSize: "14px", color: "red"}}>{error}</p>)}
         <Button>Login</Button>
         <LinkTo to="/signup">Create account</LinkTo>
       </Style.Form>
