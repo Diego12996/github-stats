b8e2770b163860a122421526c3ef4c0904418f6c
emachipana <emachipana0102@gmail.com>
2022-06-24 19:59:40 -0500

    ADD: app tests

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-
-import CardRepo from "./components/CardRepo/card-repo";
-import CardUser from "./components/CardUser";
+import LoginForm from "./components/LoginForm";
 import TitleHead from "./components/Title";
-import LinkTo from "./components/LinkTo";
 
 function App() {
   return (
     <>
       <TitleHead></TitleHead>
-      <CardUser 
-        src="https://avatars.githubusercontent.com/u/94902863?v=4"
-        name="Enmanuel Chipana"
-        username="emachipana"
-        isFavorite
-      />
-      <CardUser 
-        src="https://avatars.githubusercontent.com/u/91627176?v=4"
-        name="Lucho Zelada"
-      />
-      <CardRepo 
-        language="Ruby"
-        name="calencli"
-        description="This is a best repo"
-        stars={15}
-        forks={1}
-      />
-      <CardRepo 
-        language="CSS"
-        name="portfolio"  
-      />
-      <LinkTo to="/" >Hola</LinkTo>
+      <LoginForm />
     </>
   )
 }
