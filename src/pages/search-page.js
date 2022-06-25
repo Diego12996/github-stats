265255425bac9a5552d4fe60c3924487309e558c
emachipana <emachipana0102@gmail.com>
2022-06-25 16:56:58 -0500

    ADD: characteristics to search page

--- src/pages/search-page.js
+++ src/pages/search-page.js
@@ -1,60 +1,69 @@
 import { useEffect, useState } from "react";
 import { Input } from "../components/Input/styles";
-import { useAuth } from "../context/auth-context";
 import { useDebounce } from "react-haiku";
 import { getUser } from "../services/github-api-service";
 import Card from "../components/CardsProfile/cardsProfile";
+import * as Style from "./styles";
 
 function SearchPage() {
-  const { user } = useAuth()
-  const [query, setQuery] = useState("")
-  const [userData, setUserData] = useState([])
-  const debounceValue = useDebounce(query, 1000)
+  const [query, setQuery] = useState("");
+  const [userData, setUserData] = useState(null);
+  const debounceValue = useDebounce(query, 1000);
+
   useEffect(() => {
-    console.log(debounceValue)
-    getUser(debounceValue).then((data) => {setUserData(data)})
-  }, [debounceValue])
+    getUser(debounceValue).then((data) => {
+      setUserData(data);
+    }).catch((data) => {
+      console.error(data);
+      setUserData(null);
+    });
+  }, [debounceValue]);
 
   return (
-    <div>
-      <form>
+    <Style.UserContainer>
+      <Style.Form>
         <Input
           name="query"
           placeholder="username"
           value={query}
           onChange={(event) => setQuery(event.target.value)}
         />
-      </form>
-      <div>
-        <div>
-          <img src={userData.avatar_url} alt="gtihub-avatar"/>
-          <h1>{userData.name}</h1>
-          <p>{userData.bio}</p>
-        </div>
-      </div>
-      <div>
-        <Card
-          icon="followers"
-          number={userData.followers}
-          name="followers"
-        />
-        <Card
-          icon="following"
-          number={userData.following}
-          name="following"
-        />
-        <Card
-          icon="repos"
-          number={userData.public_repos}
-          name="public repos"
-        />
-        <Card
-          icon="gist"
-          number={userData.public_gists}
-          name="public gist"
-        />
-      </div>
-    </div>
+      </Style.Form>
+      { userData && userData.id ? (
+        <>
+        <Style.Info>
+          <Style.Avatar src={userData.avatar_url} alt="github-avatar" />
+          <Style.Name>{userData.name}</Style.Name>
+          <Style.Text>{userData.bio}</Style.Text>
+        </Style.Info>
+        <Style.Cards>
+          <Card
+            icon="followers"
+            number={userData.followers}
+            name="followers"
+          />
+          <Card
+            icon="following"
+            number={userData.following}
+            name="following"
+          />
+          <Card
+            icon="repos"
+            number={userData.public_repos}
+            name="public repos"
+          />
+          <Card
+            icon="gist"
+            number={userData.public_gists}
+            name="public gist"
+          />
+        </Style.Cards>
+        </>
+      ) 
+      : "No users..."
+    }
+      
+    </Style.UserContainer>
   )
 }
 
