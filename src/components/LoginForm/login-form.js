29be34b686674ec25a3038e00f399310f5883ccf
emachipana <emachipana0102@gmail.com>
2022-06-24 19:59:14 -0500

    ADD: login form component

---
+++ src/components/LoginForm/login-form.js
@@ -0,0 +1,34 @@
+import styled from "@emotion/styled";
+import Button from "../Button";
+import IndividualInput from "../Input";
+import LinkTo from "../LinkTo";
+
+const Form = styled.form`
+  max-width: 300px;
+  margin: 2rem auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+`;
+
+function LoginForm() {
+  return (
+    <Form>
+      <IndividualInput
+        label="Email"
+        type="email"
+        placeholder="example@mail.com"
+      />
+      <IndividualInput 
+        label="Password"
+        type="password"
+        placeholder="******"
+      />
+      <Button>Login</Button>
+      <LinkTo to="/signup">Create account</LinkTo>
+    </Form>
+  );
+}
+
+export default LoginForm;
