53bf40e4e06bfe8dc26f4e1a54692fd901c906a3
emachipana <emachipana0102@gmail.com>
2022-06-25 21:33:38 -0500

    ADD: main container

--- src/pages/followers-page.js
+++ src/pages/followers-page.js
@@ -1,18 +1,30 @@
+import styled from "@emotion/styled";
 import CardUser from "../components/CardUser";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1rem 0;
+  gap: 1rem;
+`;
+
 
 function FollowersPage({ followers }) {
   return (
     <>
     <h1>{`Followers (${followers.length})` }</h1>
-    { followers.map((foll) => (
-      <CardUser 
-        key={foll.id}
-        isFavorite={false}
-        src={foll.avatar_url}
-        name={foll.name || foll.login}
-        username={foll.login}
-      />
-    )) }
+    <Container>
+      { followers.map((foll) => (
+        <CardUser 
+          key={foll.id}
+          isFavorite={false}
+          src={foll.avatar_url}
+          name={foll.name || foll.login}
+          username={foll.login}
+        />
+      )) }
+    </Container>
     </>
   );
 };
